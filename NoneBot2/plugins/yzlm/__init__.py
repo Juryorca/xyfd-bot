ee985f2bcee63e498fdd5740f9baf96193dabb78
Juryorca <3010498997@qq.com>
2026-06-10 16:16:39 +0800

    [feat]宇宙冷漠

---
+++ NoneBot2/plugins/yzlm/__init__.py
@@ -0,0 +1,16 @@
+from nonebot import on_message
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageSegment
+
+yzlm = on_message(priority=10, block=False)
+
+
+@yzlm.handle()
+async def _(event: GroupMessageEvent):
+    text = event.get_plaintext().strip()
+
+    if "宇宙冷漠" not in text:
+        return
+
+    await matcher.finish(
+        MessageSegment.record("file:///app/napcat/musics/宇宙冷漠.mp3")
+    )
